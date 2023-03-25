a0804257223368c8f55f030628b05538982e3a2c
Soham Goswami <sohamgoswami2015@gmail.com>
2023-03-25 16:13:31 -0400

    Scatterplot of anomaly scores

--- Curriculum-Evaluator.py
+++ Curriculum-Evaluator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
 
 df = pd.read_csv('S:/University Of Windsor/Term 2/ADT/Project/Submission/curriculum_evaluator/Dataset.csv')
@@ -10,12 +11,28 @@ df = df.dropna()
 # print(df.describe())
 
 # Training the model
-# anomaly_inputs = ['CLO1', 'CLO2', 'CLO3', 'CLO4', 'CLO5', 'CLO6', 'CLO7', 'CLO8', 'CLO9']
-anomaly_inputs = ['CLO1']
+prefix = 'CLO'
+anomaly_string = ''
+anomaly_inputs = []
 model_IF = IsolationForest(contamination=float(0.1),random_state=20)
-model_IF.fit(df[anomaly_inputs])
-df['anomaly_scores'] = model_IF.decision_function(df[anomaly_inputs])
-df['anomaly'] = model_IF.predict(df[anomaly_inputs])
+# For CL01, CLO2, CLO3, CLO4, CLO5, CLO6, CLO7, CLO8 and CLO9
+for i in range(1, 10):
+    anomaly_string = f"{prefix}{i}"
+    print(anomaly_string)
+    anomaly_inputs.append(anomaly_string)
+    model_IF.fit(df[anomaly_inputs])
+    df['anomaly_scores'] = model_IF.decision_function(df[anomaly_inputs])
+    df['anomaly'] = model_IF.predict(df[anomaly_inputs])
+    subset_df = df.loc[:, [anomaly_string,'anomaly_scores','anomaly'] ]
+    subset_df.to_csv(anomaly_string + '_output.csv', index=False)
 
-print(df.loc[:, ['CLO1','anomaly_scores','anomaly'] ])
-# print(df.loc[:, ['CLO1', 'CLO2', 'CLO3', 'CLO4', 'CLO5', 'CLO6', 'CLO7', 'CLO8', 'CLO9','anomaly_scores','anomaly'] ])
+    # Creating FacetGrid object and mapping it to a scatter plot
+    g = sns.FacetGrid(df, hue='anomaly', height=5)
+    # g.map(sns.scatterplot, anomaly_string, 'anomaly_scores', s=100)
+    g.map(sns.scatterplot, 'Year', anomaly_string, s=100)
+    plt.title(anomaly_string)
+
+    # Displaying the plot
+    plt.show()
+
+    anomaly_inputs.clear()
