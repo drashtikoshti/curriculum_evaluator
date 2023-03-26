296fc28d6a424951b422abc40a568ec42568220e
Soham Goswami <sohamgoswami2015@gmail.com>
2023-03-25 23:54:49 -0400

    UI to handle user interactions with application

--- Curriculum-Evaluator.py
+++ Curriculum-Evaluator.py
@@ -9,6 +9,8 @@ Harsh Prajapati
 Drashti Koshti
 """
 
+import tkinter as tk
+from tkinter import RIGHT, Y, Scrollbar, filedialog
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -21,71 +23,86 @@ minMarks = 0
 maxMarks = 100
 clColumns = ['CLO1', 'CLO2', 'CLO3', 'CLO4', 'CLO5', 'CLO6', 'CLO7', 'CLO8', 'CLO9']
 
-df = pd.read_csv('S:/University Of Windsor/Term 2/ADT/Project/Submission/curriculum_evaluator/Dataset.csv')
+def openfile():
+    """
+       Desc: Function for uploading the user CSV file
+       param: None
+    """
+    root.filename: object = tk.filedialog.askopenfilename(initialdir="/",
+                                                          title="Select a CSV File",
+                                                          filetypes=(("csv files", "*.csv"),
+                                                                     ("all files", "*.*")))
+    return root.filename
 
-# Removing the missing values rows
-df = df.dropna()
+def getfiledata():
+    """
+        Desc: Read Selected CSV File
+        param: None
+    """
+    df = pd.read_csv(root.filename)
+    # Removing the missing values rows
+    df = df.dropna()
+    return df
 
-# -----------------------------------------------------------------------------------------------
-# Creating all CLO in one line graph
-# -----------------------------------------------------------------------------------------------
-# filter the required columns and rows
-# df = df[['Year', 'CLO1', 'CLO2', 'CLO3', 'CLO4', 'CLO5', 'CLO6', 'CLO7', 'CLO8', 'CLO9']]
-# df = df[df['Year'].between(startYear, endYear)]
-# df = df[(df[clColumns] >= 1).all(1) & (df[clColumns] <= 100).all(1)]
+def viewallCLO():
+    """
+        Desc: View All CLO data accumulated by the selected CSV as line graph
+        param: None
+    """
+    text_box.delete("1.0", tk.END)
+    text_box.insert(tk.END, "\nView All CLO data values\n\n\n")
+    # -----------------------------------------------------------------------------------------------
+    # Creating all CLO in one line graph
+    # -----------------------------------------------------------------------------------------------
+    df = getfiledata()
+    text_box.insert(tk.END, "\n\nSummary of the data from selected CSV\n\n\n")
+    text_box.insert(tk.END, df.describe())
+    # filter the required columns and rows
+    df = df[['Year', 'CLO1', 'CLO2', 'CLO3', 'CLO4', 'CLO5', 'CLO6', 'CLO7', 'CLO8', 'CLO9']]
+    df = df[df['Year'].between(startYear, endYear)]
+    df = df[(df[clColumns] >= 1).all(1) & (df[clColumns] <= 100).all(1)]
 
-# # reshape the data to long format
-# df = pd.melt(df, id_vars='Year', var_name='CLO', value_name='Value')
+    # reshape the data to long format
+    df = pd.melt(df, id_vars='Year', var_name='CLO', value_name='Value')
 
-# # plot the line chart
-# fig = px.line(df, x='Year', y='Value', color='CLO')
+    # plot the line chart
+    fig = px.line(df, x='Year', y='Value', color='CLO')
 
-# # set the axis ranges and title
-# fig.update_layout(
-#     title='CLO Performance',
-#     xaxis=dict(range=[startYear, endYear]),
-#     yaxis=dict(range=[minMarks, maxMarks])
-# )
+    # set the axis ranges and title
+    fig.update_layout(
+        title='CLO Performance',
+        xaxis=dict(range=[startYear, endYear]),
+        yaxis=dict(range=[minMarks, maxMarks])
+    )
 
-# fig.show()
-# -----------------------------------------------------------------------------------------------
+    fig.show()
+    # -----------------------------------------------------------------------------------------------
 
+def viewCLOanomaly(clo_num):
+    text_box.delete("1.0", tk.END)
+    # -----------------------------------------------------------------------------------------------
+    # Training the model
+    # -----------------------------------------------------------------------------------------------
+    prefix = 'CLO'
+    anomaly_string = f"{prefix}{clo_num}"
+    anomaly_inputs = []
+    df = getfiledata()
+    text_box.insert(tk.END, "\nView " + anomaly_string +" Anomaly Representation\n\n\n")
 
-# -----------------------------------------------------------------------------------------------
-# Training the model
-# -----------------------------------------------------------------------------------------------
-prefix = 'CLO'
-anomaly_string = ''
-anomaly_inputs = []
+    # contamination = 0.1 for expecting 10% data to be contaminated
+    # random_state = 20 as a parameter which can provide same values again
+    model_IF = IsolationForest(contamination=float(0.1),random_state=20)
 
-# Keeping contamination = 0.1 for expecting 10% data to be contaminated
-# and random_state = 20 as a parameter which can provide same values again
-model_IF = IsolationForest(contamination=float(0.1),random_state=20)
-
-# For CL01, CLO2, CLO3, CLO4, CLO5, CLO6, CLO7, CLO8 and CLO9
-for i in range(1, 10):
-    anomaly_string = f"{prefix}{i}"
     print(anomaly_string)
     anomaly_inputs.append(anomaly_string)
     model_IF.fit(df[anomaly_inputs])
     df['anomaly_scores'] = model_IF.decision_function(df[anomaly_inputs])
     df['anomaly'] = model_IF.predict(df[anomaly_inputs])
+    text_box.insert(tk.END, df[['Year', anomaly_string, 'anomaly_scores', 'anomaly']])
 
     # Storing each anomaly to a separate file as 'COL*_output.csv'
     subset_df = df.loc[:, [anomaly_string,'anomaly_scores','anomaly'] ]
     subset_df.to_csv(anomaly_string + '_output.csv', index=False)
-
-    # # Creating FacetGrid object and mapping it to a scatter plot
-    # g = sns.FacetGrid(df, hue='anomaly', height=5)
-    # # g.map(sns.scatterplot, anomaly_string, 'anomaly_scores', s=100)
-    # g.map(sns.scatterplot, 'Year', anomaly_string, s=100)
-    # plt.title(anomaly_string)
-    # # Displaying the plot
-    # plt.show()
-
-
-    # Creating a line graph of X=Year vs Y=Scores achieved in each CLO
-    # fig = px.line(df.reset_index(), x='Year', y=anomaly_string, title=anomaly_string)
 
     # Creating a scatter graph of X=Year vs Y=Scores achieved in each CLO
     fig = px.scatter(df.reset_index(), x='Year', y=anomaly_string, color='anomaly', title=anomaly_string)
@@ -96,4 +113,127 @@ for i in range(1, 10):
     fig.update_yaxes(title_text='Scores')
     fig.show()
 
-    anomaly_inputs.clear()
+
+    # Creating FacetGrid object and mapping it to a scatter plot
+    g = sns.FacetGrid(df, hue='anomaly', height=5)
+    g.map(sns.scatterplot, 'Year', anomaly_string, s=100)
+    plt.title(anomaly_string)
+    # Displaying the plot
+    plt.show()
+
+
+# GUI for the program
+root = tk.Tk()
+frame = tk.Frame(root, bg='blue')
+frame.pack()
+
+btn_open_file = tk.Button(frame,  # Button for calling upload function
+                            text='Select File',
+                            bg='dark grey',
+                            fg='blue',
+                            padx=30,
+                            pady=10,
+                            command=openfile)
+btn_open_file.pack(side=tk.LEFT)
+
+btn_view_all = tk.Button(frame,  # Button for calling viewallCLO function
+                         text="View All CLO",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=40,
+                         pady=10,
+                         command=viewallCLO)
+btn_view_all.pack(side=tk.LEFT)
+
+btn_view_clo1 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO1 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(1))
+btn_view_clo1.pack(side=tk.LEFT)
+
+btn_view_clo2 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO2 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(2))
+btn_view_clo2.pack(side=tk.LEFT)
+
+btn_view_clo3 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO3 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(3))
+btn_view_clo3.pack(side=tk.LEFT)
+
+btn_view_clo4 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO4 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(4))
+btn_view_clo4.pack(side=tk.LEFT)
+
+btn_view_clo5 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO5 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(5))
+btn_view_clo5.pack(side=tk.LEFT)
+
+btn_view_clo6 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO6 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(6))
+btn_view_clo6.pack(side=tk.LEFT)
+
+btn_view_clo7 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO7 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(7))
+btn_view_clo7.pack(side=tk.LEFT)
+
+btn_view_clo8 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO8 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(8))
+btn_view_clo8.pack(side=tk.LEFT)
+
+btn_view_clo9 = tk.Button(frame,  # Button for calling sentiment function
+                         text="View CLO9 Anomaly",
+                         bg="dark grey",
+                         fg='blue',
+                         padx=10,
+                         pady=10,
+                         command=lambda: viewCLOanomaly(9))
+btn_view_clo9.pack(side=tk.LEFT)
+
+scrollbar: Scrollbar = tk.Scrollbar(root)  # Creating a scrollbar
+scrollbar.pack(side=RIGHT, fill=Y)
+text_box = tk.Text(root, width=150, height=120, bg='lightblue', yscrollcommand=scrollbar.set)
+text_box.pack()
+scrollbar.config(command=text_box.yview)
+
+# Set the window size and position
+# root.geometry("800x600+0+0")
+root.attributes('-fullscreen', True)
+
+root.mainloop()
